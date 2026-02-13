308225922d555d3e5c8e02de5d539f70b70d93ad
Tom Curran <tomcurran5@gmail.com>
2026-02-13 00:01:33 +0000

    debug message

--- app/src/main/java/org/tomcurran/welfare/data/SyncWorker.kt
+++ app/src/main/java/org/tomcurran/welfare/data/SyncWorker.kt
@@ -1,6 +1,7 @@
 package org.tomcurran.welfare.data
 
 import android.content.Context
+import android.util.Log.d
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 
@@ -9,8 +10,13 @@ class SyncWorker(
     params: WorkerParameters,
 ) : CoroutineWorker(appContext, params) {
 
+    companion object {
+        private val TAG: String = SyncWorker::class.java.simpleName
+    }
+
     override suspend fun doWork(): Result {
         return try {
+            d(TAG, "${SyncWorker::doWork} syncing weight data")
             WeightRepository.create(applicationContext).sync()
             Result.success()
         } catch (_: Exception) {
