30e359edf7ef7218af40b91d0e706ca66d45ca9d
Tom Curran <tomcurran5@gmail.com>
2026-06-24 12:46:48 +0100

    AppLogger.entriesFlow is already a StateFlow.

--- app/src/main/java/org/tomcurran/welfare/ui/LogsViewModel.kt
+++ app/src/main/java/org/tomcurran/welfare/ui/LogsViewModel.kt
@@ -1,11 +1,8 @@
 package org.tomcurran.welfare.ui
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import org.tomcurran.welfare.data.AppLogger
 import javax.inject.Inject
 
@@ -13,7 +10,6 @@ import javax.inject.Inject
 class LogsViewModel @Inject constructor() : ViewModel() {
 
     val entries: StateFlow<List<AppLogger.LogEntry>> = AppLogger.entriesFlow
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), AppLogger.entriesFlow.value)
 
     fun clearLogs() = AppLogger.clearEntries()
 }
