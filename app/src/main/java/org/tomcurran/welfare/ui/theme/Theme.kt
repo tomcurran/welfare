1a08acb8dbbd496ceb63ad923f125a06bc66256a
Tom Curran <tomcurran5@gmail.com>
2026-06-24 09:11:24 +0100

    unneeded sdk check

--- app/src/main/java/org/tomcurran/welfare/ui/theme/Theme.kt
+++ app/src/main/java/org/tomcurran/welfare/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ fun WelfareTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
